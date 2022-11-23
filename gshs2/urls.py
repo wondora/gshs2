125c49c0ab14427ea889d3a16816d839c3966878
wondora <wondora@gmail.com>
2022-11-23 19:42:21 +0900

    edit urls

--- gshs2/urls.py
+++ gshs2/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import messages
-from django.shortcuts import redirect
 
 #URL로 MEDIA_ROOT에 접근하여 강제적으로 파일을 다운, 이미지파일 등에 접근하는 것을 방지
 # def protected_file(request, path, document_root=None):
@@ -12,7 +10,8 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gshsapp.urls')),
+    path('', include('freeboard.urls')),
+    path('gigi/', include('gshsapp.urls')),
     path('auth/', include('login.urls')),
     path('freeboard/', include('freeboard.urls')),
     path('memo/', include('memo.urls')),
