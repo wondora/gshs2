df1e9e3fb5201489881ccabdb6fd6a54f5127c31
wondora <wondora@gmail.com>
2022-11-24 13:44:47 +0900

    update static

--- gshs2/settings.py
+++ gshs2/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static')
+STATIC_ROOT = os.path.join(BASE_DIR, '.staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
